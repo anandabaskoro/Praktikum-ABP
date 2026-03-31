0d726286f28ddb6a1d5b90be42f9d4cf0daffe69
anandabaskoro <162494864+anandabaskoro@users.noreply.github.com>
2026-03-31 16:52:10 +0700

    Add files via upload

---
+++ TUTORIAL_LARAVEL1_2311102187_ANANDA BASKORO/storage/framework/views/2593a333ded2d5b2309d5a590c3255fa.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Latihan 1</title>
+</head>
+<body>
+    <h2>Praktikum Laravel</h2>
+    <p><b>Nama</b> : <?php echo e($nama); ?></p>
+    <p><b>NIM</b> : <?php echo e($nim); ?></p>
+    <p><b>Asal</b> : <?php echo e($asal); ?></p>
+</body>
+</html><?php /**PATH C:\Users\M S I\Downloads\laravel_tugas_fixed\laravel_tugas\resources\views/v_latihan1.blade.php ENDPATH**/ ?>
